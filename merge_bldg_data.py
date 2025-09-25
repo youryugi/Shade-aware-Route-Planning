09edbed79fe69718ce57e640be9bf6001a2abc15
Liuyi Yang <44858359+youryugi@users.noreply.github.com>
2025-09-25 12:44:01 +0900

    Update print statements and filename format

--- merge_bldg_data.py
+++ merge_bldg_data.py
@@ -17,7 +17,7 @@ bldg_gdf_list = [gpd.read_file(file) for file in bldg_gml_files]
 bldg_merged_gdf = pd.concat(bldg_gdf_list, ignore_index=True)
 
 
-# 2. 计算 WGS84 边界框并格式化为字符串
+
 bounds = bldg_merged_gdf.to_crs(epsg=4326).total_bounds
 minx, miny, maxx, maxy = bounds
 minx_str = f"{minx:.4f}"
@@ -25,9 +25,9 @@ miny_str = f"{miny:.4f}"
 maxx_str = f"{maxx:.4f}"
 maxy_str = f"{maxy:.4f}"
 
-# 3. 构造文件名并保存为 .pkl 文件
+
 filename = f"bldg_merged_LL_{minx_str}_{miny_str}_UR_{maxx_str}_{maxy_str}.pkl"
 save_path = rf"{filename}"
 bldg_merged_gdf.to_pickle(save_path)
 
-print(f"已保存至: {save_path}")
+print(f"saved as: {save_path}")
